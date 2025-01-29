9a658d88bb6cf3b247c1e8040d29b5ec73eacd4b
anthonyddne <igarciab20102@gmail.com>
2025-01-29 14:04:53 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Quieres ser mi San valentín y pasar tiempo juntos'.split('')
+const text = 'Quieres ser mi San valentín'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
