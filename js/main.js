ddb1c70fee8f763cd135b1470415d3f689839eac
anthonyddne <igarciab20102@gmail.com>
2025-01-30 13:08:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ onload = () => {
 
     const titles = ('Agradezco cada instante a tu lado, tu sonrisa, tus abrazos y tu amor. Contigo, todo es más bonito. Gracias por ser tú y por hacerme sentir afortunado/a.
 
-Te amo con todo mi corazón. Siempre estaré aquí para ti, porque eres mi hogar y mi destino.').split('')
+Te amo con todo mi corazón').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
